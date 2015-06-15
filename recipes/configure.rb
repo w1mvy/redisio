c4ec719b09a8e619631fa272681f04059bf0e790
w1mvy <w1mvypine@gmail.com>
2015-06-15 11:56:26 +0900

    Add supervisord job_control

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -40,19 +40,21 @@ redis_instances.each do |current_server|
   job_control = node['redisio']['job_control']
 
   if job_control == 'initd'
-  	service "redis#{server_name}" do
+    service "redis#{server_name}" do
       # don't supply start/stop/restart commands, Chef::Provider::Service::*
       # do a fine job on it's own, and support systemd correctly
       supports :start => true, :stop => true, :restart => false, :status => true
-  	end
+    end
   elsif job_control == 'upstart'
-  	service "redis#{server_name}" do
-	  provider Chef::Provider::Service::Upstart
+    service "redis#{server_name}" do
+    provider Chef::Provider::Service::Upstart
       start_command "start redis#{server_name}"
       stop_command "stop redis#{server_name}"
       restart_command "restart redis#{server_name}"
       supports :start => true, :stop => true, :restart => true, :status => false
-  	end
+    end
+  elsif job_control == 'supervisord'
+    Chef::Log.info("supervisord job control type, nothing todo.")
   else
     Chef::Log.error("Unknown job control type, no service resource created!")
   end
